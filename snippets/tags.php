2a36182c0b20866e89eded18388eb93b3761d699
Hendrik Berends <43246523+HENNIROCKS@users.noreply.github.com>
2025-11-05 16:21:43 +0100

    Add TODO and icon to tags snippet

--- snippets/tags.php
+++ snippets/tags.php
@@ -2,6 +2,11 @@
 
 /**
  * @var Kirby\Cms\Page $page
+ * 
+ * TODO:
+ * - Make tags work and remove "disabled" option
+ *   - Remove option passed to snippet in "templates/blog-article.php"
+ *   - Remove styling for class "tags__link--disabled"
  */
 
 ?>
@@ -11,10 +16,12 @@
 
         <?php if ($disabled == true): ?>
             <span class="tags__link tags__link--disabled">
+                <i class="icon icon__hashtag"></i>
                 <?= $tag ?>
             </span>
         <?php else: ?>
             <a class="tags__link" href="<?= $page->parent()->url(['params' => ['tag' => $tag]]) ?>" title="">
+                <i class="icon icon__hashtag"></i>
                 <?= $tag ?>
             </a>
         <?php endif ?>
