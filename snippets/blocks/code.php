0cad60083ae7c1acbf7bc338724fc64a8c1765ae
Hendrik Berends <43246523+HENNIROCKS@users.noreply.github.com>
2025-11-06 11:27:28 +0100

    Add code block

---
+++ snippets/blocks/code.php
@@ -0,0 +1,13 @@
+<?php
+
+/** 
+ * @var \Kirby\Cms\Block $block
+ * */
+
+?>
+
+<pre class="code">
+    <code class="code__language code__language--<?= $block->language()->or('text') ?>">
+        <?= $block->code()->html(false) ?>
+    </code>
+</pre>
